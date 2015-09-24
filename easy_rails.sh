9a70d83c463e4170e0e7b357078d68e143a6cbb8
Shanmugavel Arunachalam <iashanmugavel@users.noreply.github.com>
2015-09-24 13:46:07 +0530

    Added git package installation command

--- easy_rails.sh
+++ easy_rails.sh
@@ -1,11 +1,11 @@
 echo "Now script is going to install the necessary packages for Ruby on Rails development"
-echo "Started to update system libraries"
+echo "Started to update system libraries. Please enter password if required"
 sudo apt-get update
 
 echo "Adding build essentials"
 sudo apt-get --ignore-missing install build-essential git-core curl openssl libssl-dev libcurl4-openssl-dev zlib1g zlib1g-dev libreadline-dev libreadline6 libreadline6-dev libyaml-dev libsqlite3-dev libsqlite3-0 sqlite3 libxml2-dev libxslt1-dev python-software-properties libffi-dev libgdm-dev libncurses5-dev automake autoconf libtool bison postgresql postgresql-contrib libpq-dev pgadmin3 libc6-dev nodejs -y
 
-echo "adding node.js runtime. Please enter password if required"
+echo "adding node.js runtime"
 sudo add-apt-repository ppa:chris-lea/node.js
 
 echo "Installing ImageMagick for image processing"
@@ -21,6 +21,9 @@ rvm install ruby --latest
 echo "Installing latest version of Rails"
 gem install bundler
 gem install rails
+
+echo "Installing Git - version control system"
+sudo apt-get install git
 
 echo "Installing latest version of mysql"
 sudo apt-get install mysql-server mysql-client libmysqlclient-dev

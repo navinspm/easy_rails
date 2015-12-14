2f31698e66cd77ef25c109d27820ecd8d7dc2324
Navin kumar <navinspm@gmail.com>
2015-12-14 15:31:44 +0530

    Added key to rvm install

--- easy_rails.sh
+++ easy_rails.sh
@@ -9,6 +9,7 @@ echo "adding node.js runtime"
 sudo add-apt-repository ppa:chris-lea/node.js
 
 echo "Installing RVM (Ruby Version Manager)"
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 

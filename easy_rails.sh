cd7e2f9314752b239d49ebed7c35b1b55f15b629
gwayit <70308903+gwayit@users.noreply.github.com>
2020-10-04 17:01:05 +0530

    Updated rvm key

--- easy_rails.sh
+++ easy_rails.sh
@@ -9,8 +9,8 @@ echo "adding node.js runtime"
 sudo add-apt-repository ppa:chris-lea/node.js
 
 echo "Installing RVM (Ruby Version Manager)"
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
 echo "Installing latest version of Ruby"
